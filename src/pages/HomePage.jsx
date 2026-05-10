3f5b46d47f471330b22f503244e6f04e0a627d8c
kuzeo0227 <yzhizheng12@gmail.com>
2026-05-11 01:57:08 +0800

    HomePage: remove OurMission ('Why Oscar') section from cover stack

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,6 @@ import ProductFeature    from '../components/sections/ProductFeature'
 import Problem           from '../components/sections/Problem'
 import IngredientsShowcase from '../components/sections/IngredientsShowcase'
 import SciencePreview    from '../components/sections/SciencePreview'
-import OurMission        from '../components/sections/OurMission'
 import Footer            from '../components/sections/Footer'
 
 export default function HomePage() {
@@ -20,7 +19,6 @@ export default function HomePage() {
         <Problem />
         <IngredientsShowcase />
         <SciencePreview />
-        <OurMission />
         <Footer />
       </div>
     </main>
